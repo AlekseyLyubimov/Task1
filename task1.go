3c2f027e9c8b5cddf38ee277790bd3a1d06715b2
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2022-10-31 20:46:24 +0600

    Logging added

--- task1.go
+++ task1.go
@@ -1,51 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
 func handleRequests() {
-	http.HandleFunc("/homepage", homePage)
 	http.HandleFunc("/api/v1/xml", returnXml)
 	http.HandleFunc("/api/v1/json", returnJson)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
-	fmt.Println("Successfull start 1")
+
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(file)
+
+	log.Println("Application start")
 	handleRequests()
 }
 
 func returnXml(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnXml")
+	log.Println("Endpoint Hit: returnXml")
 	w.Write(readFromFile("return.xml"))
 }
 
 func returnJson(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnJson")
+	log.Println("Endpoint Hit: returnJson")
 	w.Write(readFromFile("return.json"))
 }
 
 func readFromFile(fileName string) []byte {
-
 	b, err := os.ReadFile(fileName) // just pass the file name
 	if err != nil {
-		fmt.Print(err)
+		log.Println(err)
 	}
-
-	fmt.Println(b) // print the content as 'bytes'
-
-	str := string(b) // convert content to a 'string'
-
-	fmt.Println(str) // print the content as a 'string'
-
 	return b
 }
