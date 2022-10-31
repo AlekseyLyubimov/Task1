69c3dcdbfcf69cfe520f8d06baec0211da339d59
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2022-10-31 19:42:28 +0600

    IO works, return actual files

--- task1.go
+++ task1.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -27,10 +26,26 @@ func main() {
 
 func returnXml(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnXml")
-	json.NewEncoder(w).Encode("this should be XML file")
+	w.Write(readFromFile("return.xml"))
 }
 
 func returnJson(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnJson")
-	xml.NewEncoder(w).Encode("this should be Json file")
+	w.Write(readFromFile("return.json"))
+}
+
+func readFromFile(fileName string) []byte {
+
+	b, err := os.ReadFile(fileName) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(b) // print the content as 'bytes'
+
+	str := string(b) // convert content to a 'string'
+
+	fmt.Println(str) // print the content as a 'string'
+
+	return b
 }
